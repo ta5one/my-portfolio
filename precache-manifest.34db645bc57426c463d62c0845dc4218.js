fb29bab042e6500ae0c959f6d16ec4427a3b4643
mo-ta-ali <tasi@live.com.au>
2023-05-11 11:45:26 +1000

    Updates

---
+++ precache-manifest.34db645bc57426c463d62c0845dc4218.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcddb387df3e7716f3f4",
+    "url": "/my-portfolio/static/css/main.a727c9f7.chunk.css"
+  },
+  {
+    "revision": "dcddb387df3e7716f3f4",
+    "url": "/my-portfolio/static/js/main.dcddb387.chunk.js"
+  },
+  {
+    "revision": "1f5025b8842fb9dd5876",
+    "url": "/my-portfolio/static/js/1.1f5025b8.chunk.js"
+  },
+  {
+    "revision": "f2bb3e8d5477afa4b7c6",
+    "url": "/my-portfolio/static/js/runtime~main.f2bb3e8d.js"
+  },
+  {
+    "revision": "495dec33607d59ccc1f488ac0ad88707",
+    "url": "/my-portfolio/index.html"
+  }
+];
